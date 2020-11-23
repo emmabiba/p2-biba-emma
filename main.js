0bb4aada15ac259aecc2508bd6ce4ec1b55c646c
Emma Biba <emmabiba@ufl.edu>
2020-11-23 06:31:56 -0600

    Header styles

--- main.js
+++ main.js
@@ -14,6 +14,26 @@ hamburger.addEventListener('click', function() {
 toggleNav();
 });
 
+//Pop Up
+
+var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
 
 //Accordion
 
